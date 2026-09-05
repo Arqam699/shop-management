03501c49d3bd67322b019a7a78fdcd83cfc702f6
Muhammad Arqam <arqammughal699@gmail.com>
2026-09-05 17:55:24 +0500

    push files

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
 // DUKAN MASTER SECURITY SWITCH
 // Default par 'false' hai (Koi bhi cheez delete nahi ho sakegi).
 // Jab aapko delete ka option chahiye ho, toh yahan 'true' likh kar save kar dein!
-export const ALLOW_GLOBAL_DELETION = true;
+export const ALLOW_GLOBAL_DELETION = false;
