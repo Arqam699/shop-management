33b0658380173b46344ac0cc5d2c33b75c5e24cd
Muhammad Arqam <arqammughal699@gmail.com>
2026-09-06 16:24:21 +0500

    push files

--- backend/routes/installmentRoutes.js
+++ backend/routes/installmentRoutes.js
@@ -1,10 +1,19 @@
 const express = require('express');
+
 const router = express.Router();
-const { getInstallmentPlans, getInstallmentPlanById, payInstallment } = require('../controllers/installmentController');
+
+const {
+  getInstallmentPlans,
+  getInstallmentPlanById,
+  payInstallment
+} = require('../controllers/installmentController');
+
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/', protect, getInstallmentPlans);
+
 router.get('/:id', protect, getInstallmentPlanById);
-router.post('/installment/:id/pay', protect, payInstallment);
+
+router.post('/:id/pay', protect, payInstallment);
 
 module.exports = router;
